9694c00314e0ec05c0a4e6b5a469c87b3cef3b85
hmmyalex@gmail.com <hmmyalex@gmail.com>
2021-03-06 18:36:01 +0200

    Added /resetsessions

---
+++ Tutorial_for_users_auth/backend/app/routes/admin/resetsessions.routes.js
@@ -0,0 +1,18 @@
+//const { verifySignUp } = require("../middleware");
+const { authJwt } = require("../../middleware");
+const controller = require("../../controllers/admin/resetsessions.controller");
+const signupController = require("../../controllers/auth.controller");
+module.exports = function(app) {
+  app.use(function(req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "x-access-token, Origin, Content-Type, Accept",
+    );
+    next();
+  });
+
+ app.get("/evcharge/api/admin/resetsessions",
+      authJwt.verifyToken,     
+      controller.resetsessions,
+      signupController.manageUser);
+};
