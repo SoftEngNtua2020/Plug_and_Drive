8e84170df0c0d21b5f0b25585bf6cd0cc1f9429d
Giannis Alexopoulos <jon@thinkpad.249.137.204>
2021-03-09 01:13:15 +0200

    add cli tests

---
+++ cli-client/addusers.py
@@ -0,0 +1,16 @@
+import requests
+import json
+def signup(username, password, email, role):
+    #url = 'https://localhost:8765/evcharge/api/login'
+    url = 'http://localhost:8080/api/auth/signup'
+    info = {'username': username, 'password': password, 'email' : email, 'roles' : [role]}
+    headers = {'Content-Type' : 'application/json'}
+    res = requests.post(url, data=info, headers=headers)
+    print(res.status_code)
+    print(res)
+    return True
+
+for i in range(1,7):
+    signup(f'alex{i}', f'password{i}', f'myemail{i}@mydb.com', 'admin')
+
+#print (args)
